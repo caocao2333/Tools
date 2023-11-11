8afabfb2c4f9d240e4d2e785a05b63f64bd41c3e
Jerry Wang <117055258+caocao2333@users.noreply.github.com>
2023-11-11 00:01:15 -0500

    Create CPUID.c

---
+++ CPUID.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int eax, ebx, ecx, edx;
+    char brand[48];
+    for (int i = 0; i < 3; i++) {
+        eax = 0x80000002 + i;
+        __asm__ volatile("cpuid"
+            : "=a" (eax),
+              "=b" (ebx),
+              "=c" (ecx),
+              "=d" (edx)
+            : "a" (eax)
+        );
+        unsigned int offset = 16 * i;
+        *(unsigned int*)(brand + offset) = eax;
+        *(unsigned int*)(brand + offset + 4) = ebx;
+        *(unsigned int*)(brand + offset + 8) = ecx;
+        *(unsigned int*)(brand + offset + 12) = edx;
+    }
+    printf("CPU Model: %s\n", brand);
+    return 0;
+}
